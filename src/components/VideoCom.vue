f7c7a7d4ae26d123e3ce2280efcb99e0e9143711
egoimmortal <want0945@gmail.com>
2023-04-12 01:52:27 +0800

    修改專案初始化

--- src/components/VideoCom.vue
+++ src/components/VideoCom.vue
@@ -1,16 +1,12 @@
-
-import { toTypeString } from '@vue/shared';
-
 <template>
-    <video src="videoSrc"></video>
+    <video></video>
+    {{ tt }}
 </template>
 
-<scrupt lang="ts" setup>
+<script lang="ts" setup>
 import { ref } from 'vue';
-
-const videoSrc = ref('https://www.youtube.com/watch?v=8_4JRK4QkqU');
-
-</scrupt>
+const tt = ref('test');
+</script>
 
 <style lang="scss" scoped>
 
